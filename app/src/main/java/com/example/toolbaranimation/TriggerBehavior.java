17949c42114abaff51de113af27fc5569359fbe4
AsiAsiAsiA <and1-1991@mail.ru>
2019-08-11 16:50:59 +0400

    First good Animation :)

--- app/src/main/java/com/example/toolbaranimation/TriggerBehavior.java
+++ app/src/main/java/com/example/toolbaranimation/TriggerBehavior.java
@@ -1,4 +1,46 @@
 package com.example.toolbaranimation;
 
-public class TriggerBehavior {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+public class TriggerBehavior extends CoordinatorLayout.Behavior<AppBarLayout> {
+
+    public TriggerBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull AppBarLayout child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return true;
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull AppBarLayout child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+    }
+
+    @Override
+    public void onNestedScroll(
+            @NonNull CoordinatorLayout coordinatorLayout,
+            @NonNull AppBarLayout child,
+            @NonNull View target,
+            int dxConsumed, int dyConsumed,
+            int dxUnconsumed, int dyUnconsumed,
+            int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+
+        Log.i("NestedScroll", "dyConsumed: " + dyConsumed);
+
+        TextView textView = coordinatorLayout.findViewById(R.id.tv_count_bonus);
+        int height = textView.getHeight();
+        textView.setHeight(height - dyConsumed);
+    }
 }
