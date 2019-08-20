d29f9b1e62cb6cfbe0deb4b008843e614a1fe0ae
Aleksandrov <and1-1991@mail.ru>
2019-08-20 21:06:39 +0400

    MotionLayout simple animation

--- app/src/main/java/com/example/toolbaranimation/MainActivity.java
+++ app/src/main/java/com/example/toolbaranimation/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.toolbaranimation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MotionLayout motion_container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_motion);
+
+        motion_container= findViewById(R.id.motion_container);
+    }
+
+    public void start(View view) {
+        motion_container.transitionToEnd();
     }
 }
